bec398bf05b2a619645615628f7cb9183593bb10
hks1 <hks1@git>
2022-05-01 18:07:15 -0400

    Vacuum Cleaner Route

--- src/main/java/com/problems/dailybyte/VacuumCleanerRoute.java
+++ src/main/java/com/problems/dailybyte/VacuumCleanerRoute.java
@@ -1,4 +1,35 @@
 package com.problems.dailybyte;
 
+// Given a string representing the sequence of moves a robot vacuum makes, return whether or not it will return to its original position. The string will only contain L, R, U, and D characters, representing left, right, up, and down respectively.
+//
+//Ex: Given the following strings...
+//
+//"LR", return true
+//"URURD", return false
+//"RUULLDRD", return true
+
 public class VacuumCleanerRoute {
+    public boolean returnToOrigPos(String route){
+        int l = 0, r = 0, u = 0, d = 0;
+        for(int i = 0; i < route.length(); i++){
+            char c = route.charAt(i);
+            if(c == 'L'){
+                l++;
+                r--;
+            }
+            if(c == 'R'){
+                r++;
+                l--;
+            }
+            if(c == 'U'){
+                u++;
+                d--;
+            }
+            if(c == 'D'){
+                u--;
+                d++;
+            }
+        }
+        return l == 0 && r == 0 && u == 0 && d == 0;
+    }
 }
--- src/test/java/com/problems/dailybyte/TestVacuumCleanerRoute.java
+++ src/test/java/com/problems/dailybyte/TestVacuumCleanerRoute.java
@@ -1,4 +1,19 @@
 package com.problems.dailybyte;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestVacuumCleanerRoute {
+    VacuumCleanerRoute obj;
+    @BeforeEach
+    public void setUp(){
+        obj = new VacuumCleanerRoute();
+    }
+
+    @Test
+    public void testReturnToOrigPos(){
+        Assertions.assertTrue(obj.returnToOrigPos("RUULLDRD"));
+        Assertions.assertFalse(obj.returnToOrigPos("URURD"));
+    }
 }
