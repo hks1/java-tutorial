64dd209a9add3790c0d95ac2b027b1373af50bfa
hks1 <hks1@users.noreply.github.com>
2022-05-09 16:00:16 -0400

    lc - subdomain visit count, daily byte - first unique character

---
+++ src/main/java/com/problems/dailybyte/FirstUniqueCharacter.java
@@ -0,0 +1,12 @@
+package com.problems.dailybyte;
+
+// Given a string, return the index of its first unique character. If a unique character does not exist, return -1.
+//
+//Ex: Given the following strings...
+//
+//"abcabd", return 2
+//"thedailybyte", return 1
+//"developer", return 0
+
+public class FirstUniqueCharacter {
+}
