6e9c54995fa4a41530011dd06a8e7358d0ad6f8d
hks1 <hks1@users.noreply.github.com>
2022-05-15 11:27:39 -0400

    trie

---
+++ src/main/java/com/trie/TrieDemo.java
@@ -0,0 +1,19 @@
+package com.trie;
+
+import java.util.Arrays;
+
+public class TrieDemo {
+    public static void main(String[] args) {
+        String[] keys = {"the", "a", "there", "answer", "any", "by", "bye", "there", "abc"};
+        String[] output = {"Not present in trie", "Present in trie"};
+        Trie t = new Trie();
+
+        System.out.println("Keys to insert: " + Arrays.toString(keys) + "\n");
+
+        int i;
+        for (i = 0; i < keys.length; i++) {
+            t.insert(keys[i]);
+            System.out.println("\"" + keys[i]+ "\"" + "inserted>");
+        }
+    }
+}
