c4f5f14d9fce376c7a542b8bccebdfc191d898ac
HKS <hks.online@gmail.com>
2023-01-08 18:54:04 -0500

    moved model classes

---
+++ src/main/java/com/hks/design/patterns/model/Computer.java
@@ -0,0 +1,14 @@
+package com.hks.design.patterns.model;
+
+public abstract class Computer {
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
+
+    @Override
+    public String toString() {
+        return "RAM: " +this.getRAM()+
+                " HDD: " + this.getHDD()+
+                " CPU: " + this.getCPU();
+    }
+}
